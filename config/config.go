8d2af3336000a6d0ed57fbd8b1f2a40ea8b5187b
Ivan <karaberov.ivan@yandex.ru>
2025-05-04 23:00:17 +0000

    feat: add load private && public keys to config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,11 @@ package config
 
 import (
 	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
 	"reflect"
 
 	"github.com/joho/godotenv"
@@ -10,14 +15,19 @@ import (
 )
 
 type Config struct {
-	DBHost     string `env:"DB_HOST"`
-	DBPort     string `env:"DB_PORT"`
-	DBUser     string `env:"DB_USER"`
-	DBPassword string `env:"DB_PASSWORD"`
-	WebhookURL string `env:"WEBHOOK_URL"`
+	DBHost                    string `env:"DB_HOST"`
+	DBPort                    string `env:"DB_PORT"`
+	DBUser                    string `env:"DB_USER"`
+	DBName                    string `env:"DB_NAME"`
+	DBPassword                string `env:"DB_PASSWORD"`
+	WebhookURL                string `env:"WEBHOOK_URL"`
+	AccessTokenExpireMinutes  int16  `env:"ACCESS_TOKEN_EXPIRE_MINUTES"`
+	RefreshTokenExpireMinutes int16  `env:"REFRESH_TOKEN_EXPIRE_MINUTES"`
+	RSAPrivateKey             *rsa.PrivateKey
+	RSAPublicKey              *rsa.PublicKey
 }
 
-func LoadConfig(ctx context.Context, filename string) *Config {
+func LoadConfig(ctx context.Context, filename string, privateRsaKeyPath string, publicRsaKeyPath string) *Config {
 	if err := godotenv.Load(filename); err != nil {
 		logrus.WithError(err).Fatal("Error loading .env file")
 	}
@@ -34,5 +44,66 @@ func LoadConfig(ctx context.Context, filename string) *Config {
 		}
 	}
 
+	rsaPrivateKey, err := loadPrivateKey(privateRsaKeyPath)
+	if err != nil {
+		logrus.WithError(err).Fatal("Error load private rsa key")
+	}
+
+	rsaPublicKey, err := loadPublicKey(publicRsaKeyPath)
+	if err != nil {
+		logrus.WithError(err).Fatal("Error load public rsa key")
+	}
+
+	cfg.RSAPrivateKey = rsaPrivateKey
+	cfg.RSAPublicKey = rsaPublicKey
+
 	return &cfg
+}
+
+func loadPrivateKey(privateKeyPath string) (*rsa.PrivateKey, error) {
+	privKeyData, err := os.ReadFile(privateKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading private key file: %v", err)
+	}
+
+	block, _ := pem.Decode(privKeyData)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block containing the private key")
+	}
+
+	privKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing PKCS#8 private key: %v", err)
+	}
+
+	rsaPrivKey, ok := privKey.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("private key is not of type *rsa.PrivateKey")
+	}
+
+	return rsaPrivKey, nil
+}
+
+func loadPublicKey(publicKeyPath string) (*rsa.PublicKey, error) {
+	pubKeyData, err := os.ReadFile(publicKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading public key file: %v", err)
+	}
+
+	block, _ := pem.Decode(pubKeyData)
+	if block == nil {
+		return nil, fmt.Errorf("failed to parse PEM block containing the public key")
+	}
+
+	pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing PKIX public key: %v", err)
+	}
+
+	rsaPubKey, ok := pubKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("public key is not of type *rsa.PublicKey")
+	}
+
+	return rsaPubKey, nil
 }
