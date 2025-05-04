294832698034c4cd87d5e5824bd3bff0dbb97d34
Ivan <karaberov.ivan@yandex.ru>
2025-05-04 22:52:49 +0000

    feat: add service for signin

--- services/auth.go
+++ services/auth.go
@@ -1,10 +1,58 @@
 package services
 
 import (
+	"simpleAuth/config"
 	"simpleAuth/models"
+	"time"
 
 	"gorm.io/gorm"
 )
+
+type UserInfo struct {
+	UserID    string
+	UserIP    string
+	UserAgent string
+}
+
+type TokenPair struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+func SignIn(db *gorm.DB, cfg *config.Config, userDetail UserInfo) (*TokenPair, error) {
+	refreshToken, err := GenerateRefreshToken()
+	if err != nil {
+		return nil, err
+	}
+
+	hashedRefreshToken, err := HashRefreshToken(refreshToken)
+	if err != nil {
+		return nil, err
+	}
+
+	session := models.Session{
+		UserID:       userDetail.UserID,
+		IP:           userDetail.UserIP,
+		UserAgent:    userDetail.UserAgent,
+		RefreshToken: hashedRefreshToken,
+		ExpireAt:     time.Now().Add(time.Duration(cfg.RefreshTokenExpireMinutes) * time.Minute),
+	}
+
+	sessionID, err := models.CreateSession(db, &session)
+	if err != nil {
+		return nil, err
+	}
+
+	accessToken, err := GenerateAccessToken(userDetail.UserID, sessionID, cfg.AccessTokenExpireMinutes, cfg.RSAPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TokenPair{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}, nil
+}
 
 func SignOut(db *gorm.DB, sessionID string) error {
 	return models.DeleteSession(db, sessionID)
